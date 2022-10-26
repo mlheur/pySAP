816442db559744ad70b9d9083375989ccfbb33b5
marc <marc_lh@hotmail.com>
2022-10-26 07:35:34 -0700

    fix: CF flag poorly set

--- alu.py
+++ alu.py
@@ -19,7 +19,7 @@ class ALU(StdRegister):
             if self.sub.istrue():
                 self.cpu.iflags['CF'].settruth(self.B.value > self.A.value)
             else:
-                self.cpu.iflags['CF'].settruth(self.value > self.mask)
+                self.cpu.iflags['CF'].settruth(((self.A.value & self.mask) + (self.B.value & self.mask)) > self.mask)
             #print("setting ZF truth to {} on value {}".format(self.value == 0, self.value))
             self.cpu.iflags['ZF'].settruth(self.value == 0)
             self.cpu.w = self.value
