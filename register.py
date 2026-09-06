b9937fdc87c996085b1edd692e0b8bb553a1625f
mlheur <mlheur@gmail.com>
2026-09-06 13:40:07 -0700

    feat: print to console in its own thread

--- register.py
+++ register.py
@@ -1,4 +1,5 @@
 from random import randint
+from threading import Thread
 
 class Register():
     def __init__(self,cpu,bits,latch=None,enable=None):
@@ -30,23 +31,33 @@ class StdRegister(Register):
 
 
 class OUT(StdRegister):
-    nROWS = 0
+    def __init__(self,cpu,latch=None,enable=None):
+        super().__init__(cpu,latch,enable)
+        self.nROWS = 0
+        self.thread = Thread(target=self.print)
+
+    def print(self):
+        if self.nROWS % 25 == 0:
+            print(f'')
+            print(f'SEQ |    BINARY |  HEX | DEC')
+            print(f'===   =========   ====   ===')
+            print(f'')
+        vBIN = f'{self.value:08b}'
+        vBIN = f'{vBIN[0:4]} {vBIN[3:7]}'
+        vHEX = f'0x{self.value:02X}'
+        vDEC = f'{self.value:03d}'
+        vSEQ = f'{self.nROWS:03d}'
+        print(f'{vSEQ}   {vBIN}   {vHEX}   {vDEC}')
+        self.nROWS += 1
+        self.thread = Thread(target=self.print)
+
     def tock(self):
         super().tock()
         if self.latch.istrue():
-            if self.nROWS % 25 == 0:
-                print(f'')
-                print(f'SEQ |    BINARY |  HEX | DEC')
-                print(f'===   =========   ====   ===')
-                print(f'')
-            vBIN = f'{self.value:08b}'
-            vBIN = f'{vBIN[0:4]} {vBIN[3:7]}'
-            vHEX = f'0x{self.value:02X}'
-            vDEC = f'{self.value:03d}'
-            vSEQ = f'{self.nROWS:03d}'
-            print(f'{vSEQ}   {vBIN}   {vHEX}   {vDEC}')
-            self.nROWS += 1
-
+            try:
+                self.thread.start()
+            except:
+                pass
 
 class PC(Register):
     def tock(self):
